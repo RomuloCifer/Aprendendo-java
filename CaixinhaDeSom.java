a58a1c3c51721ef7fefec36f8fb10dbc03141b10
Cif <ou_iy@hotmail.com>
2025-12-14 15:54:54 -0300

    refatorei para manter a lógica organizada, o main agora não precisa saber da lógica de tocar a música.

--- CaixinhaDeSom.java
+++ CaixinhaDeSom.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class CaixinhaDeSom {
     public static void main(String[] args){
+        DJ djDaFesta = new DJ();
         Scanner leitor = new Scanner(System.in, "Cp850" );
 
         System.out.println("=== CAIXINHA DE SOOOOOOOOOOOOOOOOOOOOOOM === ");
@@ -15,18 +17,22 @@ public class CaixinhaDeSom {
 
         System.out.print("Escolha um número: ");
         int escolha = leitor.nextInt();
-
+        djDaFesta.SoltarOSom(escolha);
+        leitor.close();
+    }
+}
+class DJ {
+    public void SoltarOSom(int NumeroEscolhido) {
         System.out.println("------------------ ");
-        if (escolha == 1) {
+        if (NumeroEscolhido == 1) {
             System.out.println("Rock - Tocando Galinha pintadinha - versão rock");
-        } else if (escolha == 2) {
+        } else if (NumeroEscolhido == 2) {
             System.out.println("FUNK - Tocando Baby shark - versão funk");
-        } else if (escolha == 3) {
+        } else if (NumeroEscolhido == 3) {
             System.out.println("Clássica - Tocando Gaiola das poposudas");
         } else {
             System.out.println("Opção inválida, escolha 1, 2 ou 3.");
         }
         System.out.print("------------------");
-        leitor.close();
     }
 }

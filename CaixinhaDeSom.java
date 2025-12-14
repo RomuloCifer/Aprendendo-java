6728c8e5f01cfaddd353ec47a073ea14a860cc13
Cif <ou_iy@hotmail.com>
2025-12-14 12:26:53 -0300

    Agora toca algumas músicas boas, de acordo com a escolha do usuário ;P

--- CaixinhaDeSom.java
+++ CaixinhaDeSom.java
@@ -1,16 +1,29 @@
+import java.util.Scanner;
+
 public class CaixinhaDeSom {
     public static void main(String[] args){
-        System.out.println("TESTANDOOOOO");
-        BestSongs nbs = new BestSongs();
-        nbs.PrintSongs();
-    }
-}
+        Scanner leitor = new Scanner(System.in);
 
-class BestSongs {
-    String[] songs = {"BABY", "BABY", "BABY", "OOOOOOOHHHHHHH"};
-    void PrintSongs() {
-        for (String song : songs) {
-            System.out.println(song);
+        System.out.println("=== CAIXINHA DE SOOOOOOOOOOOOOOOOOOOOOOM === ");
+        System.out.print("Qual seu nome? ");
+        String nome = leitor.nextLine();
+
+        System.out.println("Olá" + nome + "o que gostaria de ouvir hoje?");
+        System.out.print("1 - Rock");
+        System.out.print("2 - Funk");
+        System.out.print("3 - Classica");
+
+        System.out.println("Escolha um número: ");
+        int escolha = leitor.nextInt();
+
+        if (escolha == 1) {
+            System.out.print("Rock - Tocando Galinha pintadinha - versão rock");
+        } else if (escolha == 2) {
+            System.out.print("FUNK - Tocando Baby shark - versão funk");
+        } else if (escolha == 3) {
+            System.out.print("Clássica - Tocando Gaiola das poposudas");
+        } else {
+            System.out.print("Opção inválida, escolha 1, 2 ou 3.");
         }
     }
 }

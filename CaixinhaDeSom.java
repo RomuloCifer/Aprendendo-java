7f5fbc994607fec222fa48ea7fe965b759ad2bb9
Cif <ou_iy@hotmail.com>
2025-12-14 12:32:07 -0300

    Ajustando os outputs e fechando o leitor.

--- CaixinhaDeSom.java
+++ CaixinhaDeSom.java
@@ -2,28 +2,31 @@ import java.util.Scanner;
 
 public class CaixinhaDeSom {
     public static void main(String[] args){
-        Scanner leitor = new Scanner(System.in);
+        Scanner leitor = new Scanner(System.in, "Cp850" );
 
         System.out.println("=== CAIXINHA DE SOOOOOOOOOOOOOOOOOOOOOOM === ");
         System.out.print("Qual seu nome? ");
         String nome = leitor.nextLine();
 
-        System.out.println("Olá" + nome + "o que gostaria de ouvir hoje?");
-        System.out.print("1 - Rock");
-        System.out.print("2 - Funk");
-        System.out.print("3 - Classica");
+        System.out.println("Olá" + nome + " o que gostaria de ouvir hoje?");
+        System.out.println("1 - Rock");
+        System.out.println("2 - Funk");
+        System.out.println("3 - Classica");
 
-        System.out.println("Escolha um número: ");
+        System.out.print("Escolha um número: ");
         int escolha = leitor.nextInt();
 
+        System.out.println("------------------ ");
         if (escolha == 1) {
-            System.out.print("Rock - Tocando Galinha pintadinha - versão rock");
+            System.out.println("Rock - Tocando Galinha pintadinha - versão rock");
         } else if (escolha == 2) {
-            System.out.print("FUNK - Tocando Baby shark - versão funk");
+            System.out.println("FUNK - Tocando Baby shark - versão funk");
         } else if (escolha == 3) {
-            System.out.print("Clássica - Tocando Gaiola das poposudas");
+            System.out.println("Clássica - Tocando Gaiola das poposudas");
         } else {
-            System.out.print("Opção inválida, escolha 1, 2 ou 3.");
+            System.out.println("Opção inválida, escolha 1, 2 ou 3.");
         }
+        System.out.print("------------------");
+        leitor.close();
     }
 }

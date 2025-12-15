207bd42d8d20ca990b60a047290ceef80253b310
Cif <ou_iy@hotmail.com>
2025-12-14 22:42:49 -0300

    Agora o limite é definido na main. também conta quantos números são Divisíveis por três, por cinco, e por três e cinco.

--- FizzBuzz/FizzBuzz.java
+++ FizzBuzz/FizzBuzz.java
@@ -1,15 +1,28 @@
 public class FizzBuzz {
     public static void main(String[] args){
-    for (int i=1; i < 100; i++) {
-        if ( i % 5 == 0 && i % 3 == 0) {
-            System.out.println("FizzBuzz");
-        } else if (i % 3 == 0) {
-            System.out.println("Fizz");
-        } else if (i % 5 == 0) {
-            System.out.println("Buzz");
-        } else {
-            System.out.println(i + "!");
+    executarFizzBuzz(500);
+}
+    static void executarFizzBuzz(int limite) {
+        int Fizz = 0;
+        int Buzz = 0;
+        int FizzBuzz = 0;
+        for (int i=1; i < limite; i++) {
+            if ( i % 15 == 0) {
+                System.out.println("FizzBuzz");
+                FizzBuzz ++;
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+                Fizz ++;
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+                Buzz ++;
+            } else {
+                System.out.println(i + "!");
+            }
         }
+        System.out.println("Números Divisíveis por três " + Fizz );
+        System.out.println("Números divisíveis por cinco: " + Buzz);
+        System.out.println("Números divisíveis por três e por cincoDois contos: " + FizzBuzz);
     }
-    }
+    
 }
